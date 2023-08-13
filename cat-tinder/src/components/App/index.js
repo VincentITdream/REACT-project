e3000f3141f1bed528b6d2b77c5a40a1fd4f867f
wenyi-chang <wenyichang05@gmail.com>
2023-08-13 22:42:55 +1000

    Final

--- cat-tinder/src/components/App/index.js
+++ cat-tinder/src/components/App/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../HomePage';
 import CatDisplay from '../CatDisplay';
-import AdoptPage from '../Adopt'
+import AdoptPage from '../AdoptPage';
 import PageNotFound from '../PageNotFound';
 
 const App = () => {
