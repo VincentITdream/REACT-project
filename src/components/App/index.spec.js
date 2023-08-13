526b949b22eabe354420af77f8e44ff8f06daad2
wenyi-chang <wenyichang05@gmail.com>
2023-08-13 23:40:22 +1000

    done

--- src/components/App/index.spec.js
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react'
-
-import App from './'
-
-test('Render App',()=>{
-    render(<App />)
-    const element = screen.getByText('Welcome')
-    expect(element).toBeInTheDocument()
-})
